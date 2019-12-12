4f4875ffbd1af865092807769bae147e58591c27
rtshkmr <ritesh@emerald.pink>
2019-12-12 22:58:30 +0800

    working rockpaperscissors w UI elements less reset button

--- rockpaperscissors/rockpaperscissors.js
+++ rockpaperscissors/rockpaperscissors.js
@@ -23,27 +23,6 @@ function playRound(playerSelection, computerSelection) {
         return resultDisplay.textContent = "It's a draw, both chose " + array[playerSelection];
     }
 }
-function game() {
-    let playerWins = 0;
-    for (let i = 0; i < 5 && (playerWins > -3 && playerWins < 3); i++) {
-        const char = playRound(
-            prompt("choose rock, paper or scissors!"), computerPlay()).charAt(4);
-        console.log(char);
-        if (char === "W") {
-            playerWins++;
-        } else if (char === "L") {
-            playerWins--;
-        } else {
-        }
-    }
-    if (playerWins > 0) {
-        return "Congrats, you won the game, with number of successful rounds = " + playerWins + " times";
-    } else if (playerWins < 0) {
-        return "Not a good day today, lost, with number of successful rounds  " + playerWins + " times";
-    } else {
-        return "it's a draw";
-    }
-}
 // Create DOM Objects:
 // displays:
 let pointDisplay = document.querySelector(".pointsCount");
@@ -56,9 +35,7 @@ let paperButton = document.querySelector("#paper");
 let scissorButton = document.querySelector("#scissor");
 let buttons = document.querySelectorAll('button');
 
-resetButton.addEventListener('mouseover', () => {
-    counter = 0;
-});
+
 buttons.forEach( butt => {
     butt.addEventListener('click', (e) => playOnClick(e));
 
@@ -79,7 +56,7 @@ function playOnClick(e){
             winnerDisplay.textContent = "Game Over! The winner is: HUMAN"; 
         }
     } else {
-        resultDisplay.textContent = "Do you want to play again? Reset if you want";
+        resultDisplay.textContent = "Do you want to play again? Refresh Browser if you want";
 
     }
 
